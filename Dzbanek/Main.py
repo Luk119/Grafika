ea084c99907783bb58376a7e96d7e18521404629
Luk119 <lukasz.kundzicz0190@gmail.com>
2025-03-24 16:24:46 +0100

    Main dzbanek

--- Dzbanek/Main.py
+++ Dzbanek/Main.py
@@ -1,12 +1,12 @@
+import matplotlib
 import numpy as np
 import matplotlib.pyplot as plt
-import matplotlib
 from math import comb
-
-matplotlib.use('MacOSX')  # Zmień na TkAgg, jeśli masz tkinter
+matplotlib.use('MacOSX')
 
 def bernstein(n, i, t):
     return comb(n, i) * (t ** i) * ((1 - t) ** (n - i))
+
 
 def bezier_patch(control_points):
     resolution = 20
@@ -27,28 +27,60 @@ def bezier_patch(control_points):
             surface.append([px, py, pz])
     return np.array(surface)
 
-def plot_teapot(control_points):
+
+def plot_objects(teacup_points, teapot_points, spoon_points, shift_x=4.0):
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
-    for k in range(control_points.shape[0]):
-        surface = bezier_patch(control_points[k])
-        ax.plot_trisurf(surface[:, 0], surface[:, 1], surface[:, 2], color='b', alpha=0.5)
+
+    # Poprawiona orientacja filiżanki
+    teacup_points = np.copy(teacup_points)
+    teacup_points[:, :, [1, 2]] = teacup_points[:, :, [2, 1]]  # Zamiana Y i Z
+    teacup_points[:, :, 0] += shift_x  # Przesunięcie filiżanki obok dzbanka
+
+    # Przesunięcie łyżki dla lepszego widoku
+    spoon_points = np.copy(spoon_points)
+    spoon_points[:, :, 0] -= 2.0  # Przesunięcie w lewo
+
+    # Rysowanie filiżanki
+    for k in range(teacup_points.shape[0]):
+        surface = bezier_patch(teacup_points[k])
+        ax.plot_trisurf(surface[:, 0], surface[:, 1], surface[:, 2], color='black', alpha=0.5)
+
+    # Rysowanie dzbanka
+    for k in range(teapot_points.shape[0]):
+        surface = bezier_patch(teapot_points[k])
+        ax.plot_trisurf(surface[:, 0], surface[:, 1], surface[:, 2], color='navy', alpha=0.3)
+
+    # Rysowanie łyżki
+    for k in range(spoon_points.shape[0]):
+        surface = bezier_patch(spoon_points[k])
+        ax.plot_trisurf(surface[:, 0], surface[:, 1], surface[:, 2], color='pink', alpha=0.9)
+
     ax.set_xlabel("X")
     ax.set_ylabel("Y")
     ax.set_zlabel("Z")
     ax.view_init(elev=30, azim=45)
     ax.set_box_aspect([1, 1, 1])
-    plt.show(block=True)
+    plt.show()
+
 
 def read_control_points_from_txt(file_path, shape):
     with open(file_path, 'r') as file:
         lines = file.readlines()
     points = [list(map(float, line.split())) for line in lines if line.strip()]
     if len(points) != shape[0] * shape[1]:
-        raise ValueError("Nieprawidłowa liczba punktów w pliku txt " + file_path)
+        raise ValueError("Nieprawidłowa liczba punktów w pliku " + file_path)
     return np.array(points).reshape(shape)
 
-file_path = "/Users/lukaszkundzicz/PycharmProjects/Grafika/Dzbanek/punkty.txt"
-control_points = read_control_points_from_txt(file_path, (32, 16, 3))
-print(control_points.shape)  # Sprawdzenie poprawności
-plot_teapot(control_points)
+
+# Wczytanie punktów kontrolnych dla filiżanki, dzbanka i łyżki
+teacup_file = "/Users/lukaszkundzicz/PycharmProjects/Grafika/Dzbanek/punkty2"
+teapot_file = "/Users/lukaszkundzicz/PycharmProjects/Grafika/Dzbanek/punkty.txt"
+spoon_file = "/Users/lukaszkundzicz/PycharmProjects/Grafika/Dzbanek/punkty3.txt"
+
+teacup_points = read_control_points_from_txt(teacup_file, (26, 16, 3))
+teapot_points = read_control_points_from_txt(teapot_file, (32, 16, 3))
+spoon_points = read_control_points_from_txt(spoon_file, (16, 16, 3))
+
+plot_objects(teacup_points, teapot_points, spoon_points)
+
