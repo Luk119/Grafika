8c07cc0e2f15f3a12bf4936de5e8c2b0a6d00f28
Luk119 <lukasz.kundzicz0190@gmail.com>
2025-03-27 10:29:02 +0100

    Character class

---
+++ GraProjekt/Character.py
@@ -0,0 +1,13 @@
+class Character:
+    def __init__(self, x, y, image):
+        self.image = image
+        self.rect = self.image.get_rect(topleft=(x, y))
+        self.speed = 5
+        self.is_attacking = False
+
+    def move(self, dx):
+        if self.rect.colliderect(enemy.rect):
+            enemy.take_damage()
+
+    def draw(self, screen):
+        screen.blit(self.image, self.rect)
