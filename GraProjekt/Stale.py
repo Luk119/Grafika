87ee70502efcf05bbcfa2ed0087562149f1ed9cb
Luk119 <lukasz.kundzicz19@interia.pl>
2025-03-29 16:17:20 +0100

    Const class

---
+++ GraProjekt/Stale.py
@@ -0,0 +1,21 @@
+# Inicjalizacja PyGame
+import pygame
+
+pygame.init()
+
+# Ustawienia ekranu
+SZEROKOSC = 1824
+WYSOKOSC = 1200
+WYSOKOSC_PODLOGI = 1000
+ekran = pygame.display.set_mode((SZEROKOSC, WYSOKOSC))
+pygame.display.set_caption("Szczurołap 007")
+
+# Kolory
+CZARNY = (0, 0, 0)
+BIALY = (255, 255, 255)
+ZIELONY = (40, 150, 70)
+CZERWONY = (220, 30, 30)
+
+# Czcionka
+czcionka = pygame.font.SysFont('Broadway', 36)
+duza_czcionka = pygame.font.SysFont('Broadway', 72)
