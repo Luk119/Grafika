c19f769c6aa3805869be5a9e9d815e34c228a253
Luk119 <lukasz.kundzicz0190@gmail.com>
2025-04-03 12:10:05 +0200

    klasa stałe

--- GraProjekt/Stale.py
+++ GraProjekt/Stale.py
@@ -1,21 +1,17 @@
-# Inicjalizacja PyGame
+# Stałe wartości w grze
 import pygame
-
 pygame.init()
 
-# Ustawienia ekranu
 SZEROKOSC = 1124
 WYSOKOSC = 800
 WYSOKOSC_PODLOGI = 700
 ekran = pygame.display.set_mode((SZEROKOSC, WYSOKOSC))
 pygame.display.set_caption("Elvis Szczurołap")
 
-# Kolory
 CZARNY = (0, 0, 0)
 BIALY = (255, 255, 255)
 ZIELONY = (40, 150, 70)
 CZERWONY = (220, 30, 30)
 
-# Czcionka
 czcionka = pygame.font.SysFont('Arial', 36)
 duza_czcionka = pygame.font.SysFont('Arial', 72)
