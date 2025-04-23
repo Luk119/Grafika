1a1d77a75626a4dedffee6e26d3f4a743aa6c0ad
Luk119 <lukasz.kundzicz0190@gmail.com>
2025-04-23 20:45:59 +0200

    chaneg

--- Dzbanek/MainMacOs.py
+++ Dzbanek/MainMacOs.py
@@ -5,23 +5,24 @@ from math import comb
 
 matplotlib.use('MacOSX')
 
+# Funkcja Bernsteina
 def bernstein(n, i, t):
     return comb(n, i) * (t ** i) * ((1 - t) ** (n - i))
 
-
+# Funkcja generująca powierzchnię Béziera na podstawie punktów kontrolnych
 def bezier_patch(control_points):
-    # tworzenie siatki punktów
+    # Tworzenie siatki punktów
     resolution = 20
     u_vals = np.linspace(0, 1, resolution)
     w_vals = np.linspace(0, 1, resolution)
     surface = []
 
-    # iteracja po wartościach u i w w celu obliczenia współrzędnych x, y, z
+    # Iteracja po wartościach u i w, obliczanie współrzędnych x, y, z
     for u in u_vals:
         for w in w_vals:
             px, py, pz = 0.0, 0.0, 0.0
 
-            # iteracja po punktach kontrolnych
+            # Iteracja po punktach kontrolnych
             for i in range(4):
                 for j in range(4):
                     b_u = bernstein(3, i, u)
@@ -31,79 +32,73 @@ def bezier_patch(control_points):
                     py += control_points[index][1] * b_u * b_w
                     pz += control_points[index][2] * b_u * b_w
 
-            # dodanie obliczonego punktu do listy surface
+            # Dodanie obliczonego punktu do listy surface
             surface.append([px, py, pz])
 
     return np.array(surface)
 
-
+# Funkcja do rysowania obiektów 3D
 def plot_objects(teacup_points, teapot_points, spoon_points, shift_x=4.0):
-    # tworzenie figury i osi 3d
+    # Tworzenie figury i osi 3D
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
 
-    # poprawiona orientacja filiżanki
+    # Poprawiona orientacja filiżanki
     teacup_points = np.copy(teacup_points) * 1.5
-    teacup_points[:, :, [1, 2]] = teacup_points[:, :, [2, 1]]  # zamiana y i z
+    teacup_points[:, :, [1, 2]] = teacup_points[:, :, [2, 1]]  # Zamiana y i z
     teacup_points[:, :, 0] += shift_x
 
-    # przesunięcie łyżki dla lepszego widoku
+    # Przesunięcie łyżki dla lepszego widoku
     spoon_points = np.copy(spoon_points) * 2
     spoon_points[:, :, 0] -= 2.0
 
-    # rysowanie filiżanki
+    # Rysowanie filiżanki
     for k in range(teacup_points.shape[0]):
         surface = bezier_patch(teacup_points[k])
         ax.plot_trisurf(surface[:, 0], surface[:, 1], surface[:, 2], color='black', alpha=0.2)
 
-    # rysowanie dzbanka
+    # Rysowanie dzbanka
     for k in range(teapot_points.shape[0]):
         surface = bezier_patch(teapot_points[k])
         ax.plot_trisurf(surface[:, 0], surface[:, 1], surface[:, 2], color='navy', alpha=0.3)
 
-    # rysowanie łyżki
+    # Rysowanie łyżki
     for k in range(spoon_points.shape[0]):
         surface = bezier_patch(spoon_points[k])
         ax.plot_trisurf(surface[:, 0], surface[:, 1], surface[:, 2], color='pink', alpha=0.6)
 
-    # ustawienia wykresu
+    # Ustawienia wykresu
     ax.set_xlabel("X")
     ax.set_ylabel("Y")
     ax.set_zlabel("Z")
     ax.view_init(elev=30, azim=45)
-    ax.set_box_aspect([1, 1, 1])
+    ax.set_box_aspect([1, 1, 1])  # Ustawienie proporcji osi
     plt.show()
 
-
+# Funkcja do wczytywania punktów kontrolnych z pliku tekstowego
 def read_control_points_from_txt(file_path, shape):
-    # wczytywanie punktów kontrolnych z pliku tekstowego
+    # Wczytywanie punktów kontrolnych z pliku
     with open(file_path, 'r') as file:
         lines = file.readlines()
 
-    # konwersja danych do listy liczb zmiennoprzecinkowych
+    # Konwersja danych do listy liczb zmiennoprzecinkowych
     points = [list(map(float, line.split())) for line in lines if line.strip()]
 
-    # sprawdzenie poprawności liczby punktów
+    # Sprawdzenie poprawności liczby punktów
     if len(points) != shape[0] * shape[1]:
-        raise ValueError("nieprawidłowa liczba punktów w pliku " + file_path)
+        raise ValueError("Nieprawidłowa liczba punktów w pliku " + file_path)
 
     return np.array(points).reshape(shape)
 
-
-# (macos)
+# Wczytywanie punktów kontrolnych z plików
 teapot_file = "punkty.txt"
 teacup_file = "punkty2.txt"
 spoon_file = "punkty3.txt"
 
-# (windows)
-# teacup_file = "C:\\Pythonik studia\\Grafika\\Dzbanek\\punkty2.txt"
-# teapot_file = "C:\\Pythonik studia\\Grafika\\Dzbanek\\punkty.txt"
-# spoon_file = "C:\\Pythonik studia\\Grafika\\Dzbanek\\punkty3.txt"
-
-# wczytanie punktów
+# Wczytanie punktów
 teacup_points = read_control_points_from_txt(teacup_file, (26, 16, 3))
 teapot_points = read_control_points_from_txt(teapot_file, (32, 16, 3))
 spoon_points = read_control_points_from_txt(spoon_file, (16, 16, 3))
 
-# rysowanie
+# Rysowanie obiektów
 plot_objects(teacup_points, teapot_points, spoon_points)
