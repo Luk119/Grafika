c2b601a643802d902a50849e67f6647ec022a15a
Luk119 <lukasz.kundzicz0190@gmail.com>
2025-06-08 13:52:47 +0200

    done

--- PrzedKolosem/Obrazy.py
+++ PrzedKolosem/Obrazy.py
@@ -71,4 +71,26 @@ def transformacjaPotegowa(a, c):
             result.putpixel((i, j), (r, g, b))
     result.show()
 
-transformacjaPotegowa(0.2,0.91)
+def filtry():
+    img = Image.open("auto.jpg")
+    w, h = img.size
+    filter=[[0, 0, 1], [1, 2, 3], [-2, -2, 3]]
+    result = Image.new(('RGB'), (w, h))
+    for i in range(1, w-1):
+        for j in range(1, h-1):
+            tmp_r = 0
+            tmp_g = 0
+            tmp_b = 0
+            for k in range(-1, 2):
+                for l in range(-1, 2):
+                    r, g, b = img.getpixel((i+k, j+l))
+                    tmp_r = r * filter[k+1][l+1]
+                    tmp_g = g * filter[k+1][l+1]
+                    tmp_b = b * filter[k+1][l+1]
+            tmp_r = max(min(tmp_r, 255), 0)
+            tmp_g = max(min(tmp_g, 255), 0)
+            tmp_b = max(min(tmp_b, 255), 0)
+            result.putpixel((i, j), (tmp_r, tmp_g, tmp_b))
+    result.show()
+
+filtry()
